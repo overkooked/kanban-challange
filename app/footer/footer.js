c4b3a94c957cab5cfae3205b6589b6ccd56e4b2f
Anup Saw <anupsaw@gmail.com>
2021-04-20 02:02:15 -0400

    changes

---
+++ app/footer/footer.js
@@ -0,0 +1,11 @@
+// (function () {
+
+//     const footer = document.getElementById('kbFooter');
+//     fetch('./app/partials/footer.html').then(x => console.log(x));
+//     console.log(footer);
+// })();
+
+// import html from './footer1.html';
+// console.log(html);
+
+console.log(document.getElementById('xyz'))
